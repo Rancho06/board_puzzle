36fde2c528450f6b79db2beb5e27ab386a1c72dd
Shu Zhou <shuzhou@usc.edu>
2013-03-20 05:29:00 -0700

    complete PuzzleSolver Class. close #7

--- puzzle_solver.cpp
+++ puzzle_solver.cpp
@@ -7,9 +7,67 @@
 
 using namespace std;
 
+PuzzleSolver::PuzzleSolver(const Board &b){
+	b_=b;
+	expansions_=0;
+}
 
+PuzzleSolver::~PuzzleSolver(){
+}
+
+int PuzzleSolver::run(PuzzleHeuristic* ph){
+	BoardSet closedlist;
+  PMMinList openlist;
+  vector<PuzzleMove*> garbage;
+  PuzzleMove* pm=new PuzzleMove(b_);
+  openlist.push(pm);
+  while(!openlist.empty()){
+  	PuzzleMove* move=openlist.top();
+  	openlist.pop();
+  	closedlist.insert(move->b_);
+  	garbage.push_back(move);
+  	if(move->b_->solved()){
+  		//trace
+  		do{
+  			solutionlist.push_back(move->tileMove_);
+  			move=move->prev_;
+  		}
+  		while(move!=NULL);
+  		
+  		break;
+  	}
+  	std::map<int, Board*> pmove=move->b_->potentialMoves();
+  	std::map<int, Board*>::iterator it;
+  	for(it=pmove.begin();it!=pmove.end();++it){
+  		PuzzleMove* pm2=new PuzzleMove(it->first,it->second,move);
+  		if(closedlist.find(pm2->b_)==closedlist.end()){
+  			pm2->h_=ph->compute(pm2->b_->getTiles(),pm2->b_->getSize());
+  			openlist.push(pm2);
+  			expansions_++;
+  		}
+  		else delete pm2;
+  	}
+  }
+  vector<PuzzleMove*>::iterator it;
+  for(it=garbage.begin();it!=garbage.end();++it){
+  	delete (*it);
+  }
+ 	
+ 	while(!openlist.empty()){
+ 		delete openlist.top();
+ 		openlist.pop();
+ 	}
+  
+  return solutionlist.size();
+}
 
 int PuzzleSolver::getNumExpansions()
 {
   return expansions_;
 }
+
+MyList<int> PuzzleSolver::getlist(){
+	return solutionlist;
+}
+
+
