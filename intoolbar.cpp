6f5f91e3d3986dcd876d96540f8ef68f51707573
Shu Zhou <shuzhou@usc.edu>
2013-04-14 14:33:16 -0700

    close #17. close #22

--- intoolbar.cpp
+++ intoolbar.cpp
@@ -24,12 +24,66 @@ InToolBar::InToolBar(MainWindow* m_w,QApplication* q_a):QToolBar(){
 }
 
 void InToolBar::startGame(){
-
 	GraphicsWindow* gw=mw->getgraphwindow();
+	gw->resetheur();
+	//mw->gethorizon()->resetbutton();
+	gw->getscene()->clear();
+	gw->getscreentiles()->clear();
+	mw->getlist()->clear();
+	mw->getErrorBox()->clear();
+	
 	MyLayout *layout=mw->getlayout();
-	gw->setsize(layout->getsize());
-	gw->setnum(layout->getnum());
-	gw->setseed(layout->getseed());
+	int size=layout->getsize();
+	
+	if(size==-99){
+		layout->reset();
+		mw->getErrorBox()->append("You must enter an integer as the size. \nPlease enter again.");
+		return;
+	}
+	else if(size<=0){
+		layout->reset();
+		mw->getErrorBox()->append("You must enter a positive number as the size.\nPlease enter again.");
+		return;
+	}
+	else if (size!=4&&size!=9&&size!=16&&size!=25&&size!=36){
+		layout->reset();
+		mw->getErrorBox()->append("You must enter a square number as the size.\nPlease enter again.");
+		return;
+	}
+	else{
+		gw->setsize(size);
+	}
+	
+	int num=layout->getnum();
+	if(num==-99){
+		layout->reset();
+		mw->getErrorBox()->append("You must enter an integer as the initial moves of the board.\n Please enter again.");
+		return;
+	}
+	else if(num<=0){
+		layout->reset();
+		mw->getErrorBox()->append("You must enter a positive number as the initial moves of the board.\n Please enter again.");
+		return;
+	}
+	else{
+		gw->setnum(num);
+	}
+	
+	int seed=layout->getseed();
+	if(seed==-99){
+		layout->reset();
+		mw->getErrorBox()->append("You must enter an integer as the random seed.\n Please enter again.");
+		return;
+	}
+	else if(seed<=0){
+		layout->reset();
+		mw->getErrorBox()->append("You must enter a positive number as the random seed.\n Please enter again.");
+		return;
+	}
+	else{
+		gw->setseed(seed);
+	}
+	layout->reset();
 	int dim=sqrt(gw->getsize());
 	gw->setmyboard();
 	for(int i=0;i<gw->getsize();i++){
@@ -42,23 +96,28 @@ void InToolBar::startGame(){
  
 }
 
+
 void InToolBar::quitGame(){
 	qa->exit(0);
 }
 
 void InToolBar::run(){
+	if(mw->getgraphwindow()->getscreentiles()->size()==0){
+		mw->getErrorBox()->append("Please start a game first!");
+		return;
+	}
+	if(mw->getgraphwindow()->getheur()==NULL){
+		mw->getErrorBox()->append("You must select a heuristic option before running the Cheat! ");
+		return;
+	}
+	mw->getlist()->clear();
+	mw->getErrorBox()->clear();
 	QStringList* list;
-	ManhattanHeuristic* mh=new ManhattanHeuristic;
 	Board b=*(mw->getgraphwindow()->getboard());
 	PuzzleSolver ps(b);
-	ps.run(mh);
+	ps.run(mw->getgraphwindow()->getheur());
 	int size=ps.getlist()->size();
 	list=ps.getqstring();
 	list->push_front("Try the following sequence:");
 	mw->getlist()->insertItems(0,*list);
-	
-	
-
-
-
 }
