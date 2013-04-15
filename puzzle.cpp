1c072352a3d7ca4f7f3b697d0a56f70b98946e2b
Shu Zhou <shuzhou@usc.edu>
2013-04-14 20:26:24 -0700

    clean staged.

--- puzzle.cpp
+++ puzzle.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <cstdlib>
 #include <deque>
 #include "board.h"
@@ -7,17 +7,18 @@
 
 using namespace std;
 
-/** The main function of the whole program to parse the commandline information 
+The main function of the whole program to parse the commandline information 
   it will call the move() function to move a tile or 
   call the run() method to provide a cheat
-  @mainpage csci102 Programming Assignment #3
-  @section purpose Purpose/Overview
+   csci102 Programming Assignment #3
+   purpose Purpose/Overview
   This assignment will allow a user to select the size of the game board, 
   though we will require it to be square. Valid sizes are 4 (2x2), 9 (3x3), 16 (4x4), etc. 
   Further, we will define the SOLVED gameboard to be as shown below with the blank tile at the upper-left corner.
-  @section requirements Requirements
+   requirements Requirements
   The program requires users to select a valid tile to move or choose -1 for a cheat.
 */
+/*
 int main(int argc, char *argv[])
 {
   	if(argc < 4){
@@ -34,11 +35,11 @@ int main(int argc, char *argv[])
   
   	int tile;
   	cout<<b;
-  	ManhattanHeuristic* mh=new ManhattanHeuristic;
+  	ManhattanHeuristic* mh=new ManhattanHeuristic;*/
   	/** if the board has been solved, the program ends
   	 * if the board is not solved, we ask for a tile to move or provide a cheat.
   	*/
-  	while(!b.solved()){
+  	/*while(!b.solved()){
 		do{
 			cout<<"Enter tile number to move or -1 for a cheat: ";
 			cin>>tile;
@@ -51,11 +52,11 @@ int main(int argc, char *argv[])
 			}
 			break;
 		}
-		while(true);
+		while(true);*/
 		/** if the user choose to cheat, we use the run() in PuzzleSolver class 
 		 * to get the sequence of numbers
 		*/
-		if(tile==-1){
+		/*if(tile==-1){
 			int step;
 			MyList<int>* list;
 			PuzzleSolver ps(b);
@@ -68,12 +69,12 @@ int main(int argc, char *argv[])
 			}
 			cout<<endl<<"(Expansions = "<<ps.getNumExpansions()<<")\n";
 			cout<<b;
-		}
+		}*/
 		/** if the user chooses to move an actual tile, we first check whether the tile can be moved.
 		 * if the tile can be moved, we move that.
 		 * if the tile can not be moved, we print a error message and continue the game
 		*/
-		else{
+		/*else{
 			std::map<int,Board*> plist=b.potentialMoves();
 			//Check whether the tile entered can be moved
 			if( plist.find(tile) != plist.end()){
@@ -96,4 +97,4 @@ int main(int argc, char *argv[])
 	}
 	delete mh;
   return 0;
-}
+}*/
